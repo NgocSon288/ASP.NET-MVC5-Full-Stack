972d908e5803d588d42c4637316f4d9c3cfc6383
Huynh Ngoc Son <18521694@gm.uit.edu.vn>
2021-02-22 23:10:51 +0700

    Commit Permission page -  CRUI, search

---
+++ FShop/FShop.Web/Areas/Admin/Controllers/PermissionController.cs
@@ -0,0 +1,86 @@
+﻿using FShop.Model.Models;
+using FShop.Service.Services;
+using FShop.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FShop.Web.Areas.Admin.Controllers
+{
+    public class PermissionController : BaseAdminController
+    {
+        private readonly IPermissionService _permissionService;
+
+        public PermissionController(IPermissionService PermissionService)
+        {
+            this._permissionService = PermissionService;
+        }
+
+        // GET: Admin/Permission
+        public ActionResult Index()
+        {
+            return View(AutoMapper.Mapper.Map<List<PermissionViewModel>>(_permissionService.GetAll()));
+        }
+
+        [HttpPost]
+        public ActionResult Add(string id, string name, string description)
+        {
+            try
+            {
+                var permission = new Permission();
+
+                permission.ID = id;
+                permission.Name = name;
+                permission.Description = description;
+
+                _permissionService.Insert(permission);
+                _permissionService.SaveChanges();
+
+                return Content("1");
+            }
+            catch (Exception)
+            {
+                return Content("0");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Update(string id, string name, string description)
+        {
+            try
+            {
+                var permission = _permissionService.GetAll().FirstOrDefault(p => p.ID == id);
+
+                permission.Name = name;
+                permission.Description = description;
+
+                _permissionService.Update(permission);
+                _permissionService.SaveChanges();
+
+                return Content("1");
+            }
+            catch (Exception)
+            {
+                return Content("0");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Delete(string id)
+        {
+            try
+            {
+                _permissionService.Delete(_permissionService.GetAll().FirstOrDefault(p => p.ID == id));
+                _permissionService.SaveChanges();
+
+                return Content("1");
+            }
+            catch (Exception)
+            {
+                return Content("0");
+            }
+        }
+    }
+}
---
+++ FShop/FShop.Web/Assets/Admin/js/MyScript/PermissionScript.js
@@ -0,0 +1,187 @@
+﻿var permission = {
+    init: function () {
+        this.addRow();
+        this.hiddenNewRow();
+        this.addPermission();
+        this.clickEdit();
+        this.clickCloseUpdate();
+        this.clickUpdate();
+        this.clickDelete();
+        this.clickSearch();
+    },
+    addRow: function () {
+        $(".btn-add-row").off("click").on("click", function () {
+            $("#new-row").removeClass("hidden");
+        })
+    },
+    hiddenNewRow: function () {
+        $(".btn-delete-new-row").off("click").on("click", function () {
+            $(this).parents("tr").addClass("hidden");
+            $("#txt-id").val("");
+            $("#txt-name").val("");
+            $("#txt-description").val("");
+        })
+    },
+    addPermission: function () {
+        $(".btn-add-permission").off("click").on("click", function () {
+            var id = $("#txt-id").val();
+            var name = $("#txt-name").val();
+            var description = $("#txt-description").val();
+            var check = true;
+
+            if (!id) {
+                $("#txt-id").addClass("input-border-validate");
+                check = false;
+            } else {
+                $("#txt-id").removeClass("input-border-validate");
+                check = true;
+            }
+
+            if (!name) {
+                $("#txt-name").addClass("input-border-validate");
+                check = false;
+            } else {
+                $("#txt-name").removeClass("input-border-validate");
+                check = true;
+            }
+
+            if (!description) {
+                $("#txt-description").addClass("input-border-validate");
+                check = false;
+            } else {
+                $("#txt-description").removeClass("input-border-validate");
+                check = true;
+            }
+
+            if (check) {
+                $("#new-row").addClass("hidden");
+                $("#txt-id").val("");
+                $("#txt-name").val("");
+                $("#txt-description").val("");
+
+                $.ajax({
+                    url: "/Admin/Permission/Add",
+                    data: { id : id, name: name, description: description },
+                    type: "POST",
+                    success: function (response) {
+                        if (response == "0") {
+                            alert("Lỗi");
+                        } else {
+                            window.location.reload();
+                        }
+                    }
+                })
+            }
+        })
+    },
+    clickEdit: function () {
+        $(".btn-edit").off("click").on("click", function () {
+            var parent = $(this).parents("tr");
+            var id = parent.find(".lbl-id").text();
+            var name = parent.find(".lbl-name").text();
+            var description = parent.find(".lbl-description").text();
+
+            parent.prev().find(".txt-id").val(id);
+            parent.prev().find(".txt-name").val(name);
+            parent.prev().find(".txt-description").val(description);
+
+            parent.addClass("hidden");
+            parent.prev().removeClass("hidden");
+        })
+    },
+    clickCloseUpdate: function () {
+        $(".btn-close-update").off("click").on("click", function () {
+            $(this).parents("tr").addClass("hidden");
+            $(this).parents("tr").next().removeClass("hidden");
+        })
+    },
+    clickUpdate: function () {
+        $(".btn-update").off("click").on("click", function () {
+            var parent = $(this).parents("tr");
+
+            var id = parent.find(".txt-id").val();
+            var name = parent.find(".txt-name").val();
+            var description = parent.find(".txt-description").val();
+            var check = true;
+
+            if (!id) {
+                $("#txt-id").addClass("input-border-validate");
+                check = false;
+            } else {
+                $("#txt-id").removeClass("input-border-validate");
+                check = true;
+            }
+
+            if (!name) {
+                $("#txt-name").addClass("input-border-validate");
+                check = false;
+            } else {
+                $("#txt-name").removeClass("input-border-validate");
+                check = true;
+            }
+
+            if (!description) {
+                $("#txt-description").addClass("input-border-validate");
+                check = false;
+            } else {
+                $("#txt-description").removeClass("input-border-validate");
+                check = true;
+            }
+
+            if (check) {
+                $("#new-row").addClass("hidden");
+                $("#txt-id").val("");
+                $("#txt-name").val("");
+                $("#txt-description").val("");
+
+                $.ajax({
+                    url: "/Admin/Permission/Update",
+                    data: { id: id, name: name, description: description },
+                    type: "POST",
+                    success: function (response) {
+                        if (response == "0") {
+                            alert("Lỗi");
+                        } else {
+                            window.location.reload();
+                        }
+                    }
+                })
+            }
+        })
+    },
+    clickDelete: function () {
+        $(".btn-delete").off("click").on("click", function () {
+            var id = $(this).data("id");
+
+            $.ajax({
+                url: "/Admin/Permission/Delete",
+                data: { id: id },
+                type: "POST",
+                success: function (response) {
+                    if (response == "0") {
+                        alert("Lỗi");
+                    } else {
+                        window.location.reload();
+                    }
+                }
+            })
+        })
+    },
+    clickSearch: function () {
+        $(".btn-search").off("click").on("click", function (e) { 
+            var search = $("#txt-search").val().toUpperCase();
+
+            $("tbody").find(".tr-main").each(function (index, element) {
+                var text = $(element).text().toUpperCase();  
+
+                if (text.indexOf(search) < 0) {
+                    $(element).addClass("hidden");
+                } else {
+                    $(element).removeClass("hidden");
+                }
+            })
+        })
+    }
+}
+
+permission.init();
